9ec0bd40606b7109ff4804e73a3df487d8b0a333
Bhavesh <goswamibhavesh22@gmail.com>
2025-09-16 22:24:26 +0530

    new files added

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    // best selling
+    $('.best-selling-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 3 // better for tablet view
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2 // better for tablet view
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1 // best for mobile
+                }
+            }
+        ]
+    });
+    // Treding product slider
+    $('.treding-product-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 3 // better for tablet view
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2 // better for tablet view
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1 // best for mobile
+                }
+            }
+        ]
+    });
+});
